7971d2c9c746e26fd2f3ebee468f23e621f73e0e
Kinga <kinga.stanek@shijigroup.com>
2019-10-27 15:17:26 +0100

    Add basic Registration UI

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
-import { Grid } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
-import styles from './Registration.style'; 
+import { Field, reduxForm } from "redux-form";
+import { Grid, Button } from "@material-ui/core";
+import strings from "config/strings";
+import { withStyles } from "@material-ui/styles";
+import styles from "./Registration.style";
 
 class Registration extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, handleSubmit } = this.props;
     return (
-      <Grid className={classes.wrapper}>
-       Registration
-      </Grid>
+      <form onSubmit={handleSubmit} className={classes.formWrapper}>
+        <Grid className={classes.inputWrapper}>
+          <label className={classes.formLabel}>{strings.REGISTRATION}</label>
+          <Field
+            name="registration"
+            component="input"
+            type="text"
+            className={classes.field}
+          />
+        </Grid>
+        <Grid className={classes.inputWrapper}>
+          <label className={classes.formLabel}>{strings.PASSWORD}</label>
+          <Field
+            name="password"
+            component="input"
+            type="password"
+            className={classes.field}
+          />
+        </Grid>
+        <Button
+          type="submit"
+          classes={{
+            root: classes.buttonRoot,
+            label: classes.buttonLabel
+          }}
+        >
+          {strings.REGISTRATION}
+        </Button>
+      </form>
     );
   }
 }
 
-export default withStyles(styles)(Registration);
+export default reduxForm({
+  form: "registraion"
+})(withStyles(styles)(Registration));
