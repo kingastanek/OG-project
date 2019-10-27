bacf719e27bb7f8793c9904b1d64fcb0f6d7db6f
Kinga <kinga.stanek@shijigroup.com>
2019-10-27 11:49:01 +0100

    Add getUserResources action and reducer

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,35 @@
+import {
+  GET_USER_RESOURCES_REQUEST,
+  GET_USER_RESOURCES_SUCCESS,
+  GET_USER_RESOURCES_FAILURE,
+} from "store/actions/action";
+
+const initialState = {
+  areResourcesFetching: false,
+  resources: {}
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_USER_RESOURCES_REQUEST: {
+      return Object.assign({}, state, {
+        areResourcesFetching: true
+      });
+    }
+    case GET_USER_RESOURCES_SUCCESS: {
+      const result = action;
+      return Object.assign({}, state, {
+        areResourcesFetching: false,
+        resources: result
+      });
+    }
+    case GET_USER_RESOURCES_FAILURE: {
+      return Object.assign({}, state, {
+        areResourcesFetching: false
+      });
+    }
+
+    default:
+      return state;
+  }
+};
