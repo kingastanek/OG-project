fb60cbea191762bb9ee142fce87d748591071d4c
kingastanek <kin.stanek@gmail.com>
2020-02-09 20:34:09 +0100

    Add BuildingTabsSection component

---
+++ src/components/BuildingsTabsSection/BuildingsTabsSection.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/styles";
+import styles from "./BuildingsTabsSection.style";
+
+class BuildingDetailsCard extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default withStyles(styles)(BuildingDetailsCard);
