94ef5d93a090c4bb9b7e80b90936b6b5aaf8f63b
kingastanek <kin.stanek@gmail.com>
2020-02-11 23:17:49 +0100

    Change Main class component to functional component

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
 import Menu from "components/Menu";
@@ -9,31 +9,28 @@ import welcome from "assets/images/welcome.jpg";
 import styles from "./Main.style";
 import strings from "config/strings";
 
-class Main extends Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Grid container className={classes.container}>
-        <TopResourcesPanel />
-        <Grid item xs={12} className={classes.contentWrapper}>
-          <Grid item xs={2}><Menu /></Grid>
-          <Grid item xs={8} className={classes.actionBarContainer}>
-            <Grid className={classes.centeredWrapper}><ActionBar /></Grid>
-            <Grid className={classes.centeredWrapper}>
-              <img src={welcome} alt="welcome" className={classes.welcomeImg} />
-              <div className={classes.welcomeTextBlock}>
-                <Typography className={classes.welcomeText}>
-                  {strings.WELCOME_TEXT}
-                </Typography>
-              </div>
-            </Grid>
+const main = (props) => {
+  const { classes } = props;
+  return (
+    <Grid container className={classes.container}>
+      <TopResourcesPanel />
+      <Grid item xs={12} className={classes.contentWrapper}>
+        <Grid item xs={2}><Menu /></Grid>
+        <Grid item xs={8} className={classes.actionBarContainer}>
+          <Grid className={classes.centeredWrapper}><ActionBar /></Grid>
+          <Grid className={classes.centeredWrapper}>
+            <img src={welcome} alt="welcome" className={classes.welcomeImg} />
+            <div className={classes.welcomeTextBlock}>
+              <Typography className={classes.welcomeText}>
+                {strings.WELCOME_TEXT}
+              </Typography>
+            </div>
           </Grid>
-          <Grid item xs={2}><Planets /></Grid>
         </Grid>
+        <Grid item xs={2}><Planets /></Grid>
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
 
-export default withStyles(styles)(Main);
+export default withStyles(styles)(main);
