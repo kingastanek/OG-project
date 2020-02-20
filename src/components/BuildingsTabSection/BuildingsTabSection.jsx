b5a0191866de9fd70d7259722cf08e91b9a192c6
kingastanek <kin.stanek@gmail.com>
2020-02-20 21:59:13 +0100

    Add new component ResearchTabSection

---
+++ src/components/BuildingsTabSection/BuildingsTabSection.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Grid, Paper } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+import styles from "./BuildingsTabSection.style";
+
+class BuildingsTabSection extends Component {
+  render() {
+    const {
+      classes,
+      className,
+      onClick,
+      notAbleToBuild,
+      style,
+      timeLayerStyle,
+      minutes,
+      hours,
+      seconds,
+    } = this.props;
+    return (
+      <Grid className={classes.buildingsMines}>
+      <Paper
+        className={className}
+        onClick={onClick}
+        style={style}
+      >
+        {notAbleToBuild && (
+          <React.Fragment>
+            <div className={classes.timeRemaining}>{hours}h {minutes}m {seconds}s</div>
+            <div className={classes.timeLayer} style={timeLayerStyle} />
+          </React.Fragment>
+        )} 
+      </Paper>
+    </Grid>
+    )
+  }
+}
+
+export default withStyles(styles)(BuildingsTabSection);
--- src/components/ResearchTabsSection/ResearchTabSection.jsx
+++ src/components/ResearchTabsSection/ResearchTabSection.jsx
@@ -1,38 +1,14 @@
 import React, { Component } from "react";
-import { Grid, Paper } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
-import styles from "./BuildingsTabSection.style";
+import styles from "./ResearchTabSection.style";
 
-class BuildingDetailsCard extends Component {
+class ResearchTabSection extends Component {
   render() {
-    const {
-      classes,
-      className,
-      onClick,
-      notAbleToBuild,
-      style,
-      timeLayerStyle,
-      minutes,
-      hours,
-      seconds,
-    } = this.props;
+    
     return (
-      <Grid className={classes.buildingsMines}>
-      <Paper
-        className={className}
-        onClick={onClick}
-        style={style}
-      >
-        {notAbleToBuild && (
-          <React.Fragment>
-            <div className={classes.timeRemaining}>{hours}h {minutes}m {seconds}s</div>
-            <div className={classes.timeLayer} style={timeLayerStyle} />
-          </React.Fragment>
-        )} 
-      </Paper>
-    </Grid>
+      <div></div>
     )
   }
 }
 
-export default withStyles(styles)(BuildingDetailsCard);
+export default withStyles(styles)(ResearchTabSection);
